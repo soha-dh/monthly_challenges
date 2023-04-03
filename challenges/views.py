d796c31837fdcab0aabaf8eb883e0f29ca8208db
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-04-04 00:32:45 +0330

    end 22

--- challenges/views.py
+++ challenges/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "this is january",
+    "february": "this is February",
+    "march": "this is march",
+    "april": "this is april",
+    "may": "this is may",
+    "june": "this is june",
+    "julay": "this is julay"
+}
 
-# def january(request):
-#     return HttpResponse("this works !")
 
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
 
-# def february(request):
-#     return HttpResponse("this works on February !")
+    if month > len(months):
+        return HttpResponseNotFound('Invalid month')
+
+    redirect_month = months[month]
+    return HttpResponseRedirect("/challenges/"+redirect_month)
+
 
 def monthly_challenge(request, month):
-    reply_text = None
-    if month == "january":
-        reply_text = "this is january month"
-    elif month == "february":
-        reply_text = "this is february month"
-    elif month == "march":
-        reply_text = "this is march"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound(" we are not support this month")
-
-    return HttpResponse(reply_text)
