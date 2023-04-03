e89aeeb8e23ec721006618c704094dd4430b0386
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-04-03 21:11:48 +0330

    end 19

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("this works !")
+# def january(request):
+#     return HttpResponse("this works !")
+
+
+# def february(request):
+#     return HttpResponse("this works on February !")
+
+def monthly_challenge(request, month):
+    reply_text = None
+    if month == "january":
+        reply_text = "this is january month"
+    elif month == "february":
+        reply_text = "this is february month"
+    elif month == "march":
+        reply_text = "this is march"
+    else:
+        return HttpResponseNotFound(" we are not support this month")
+
+    return HttpResponse(reply_text)
