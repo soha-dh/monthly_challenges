10f0248272c64b477b94d6846a899d6c633db48c
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-04-04 18:31:44 +0330

    end 25

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-
+from django.urls import reverse
 # Create your views here.
 
 monthly_challenges = {
@@ -20,8 +20,9 @@ def monthly_challenge_by_number(request, month):
     if month > len(months):
         return HttpResponseNotFound('Invalid month')
 
-    redirect_month = months[month]
-    return HttpResponseRedirect("/challenges/"+redirect_month)
+    redirect_month = months[month-1]
+    ect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(ect_path)
 
 
 def monthly_challenge(request, month):
@@ -30,3 +31,17 @@ def monthly_challenge(request, month):
         return HttpResponse(challenge_text)
     except:
         return HttpResponseNotFound(" we are not support this month")
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
